cdef413de7e7d71c17e08d6dec4f893a89c02507
Jake Orton <116884079+Jxkeorton@users.noreply.github.com>
2024-10-18 14:52:58 +0000

    Add code snippet shortcuts

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,57 @@
+// // rfce
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// // rafce
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// // rce
+// import React, { Component } from 'react'
+
+// export class 
+
+// extends Component {
+//   render() {
+//     return (
+//       <div>
+        
+//       </div>
+//     )
+//   }
+// }
+
+// export default 
+
+// // rconst
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+     
+//   }
+// }
+
+// // clg
+// console.log(object)
+
+
+
