e8f40349dd273be36613dbfb0f82d0528d8ff9da
Jake Orton <116884079+Jxkeorton@users.noreply.github.com>
2024-10-18 15:45:04 +0000

    Nesting components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,18 +6,20 @@ import EventsFunctional from './components/EventsFunctional';
 import EventBinding from './components/EventBinding';
 import ConditionalRenderingClass from './components/ConditionalRenderingClass';
 import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
+import NestingComponents from './components/NestingComponents';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <ConditionalRenderingClass />
+      <NestingComponents />
+      {/* <ConditionalRenderingClass />
       <ConditionalRenderingFunctional connected={true} />
       <EventBinding />
       <EventsClass />
       <EventsFunctional />	
       <NavBarSimple />
-      <Sidebar />
+      <Sidebar /> */}
     </div>
   );
 }
