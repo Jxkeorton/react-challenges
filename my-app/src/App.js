c35f786123bdd1899b4780e86c5f3e4e927a85d3
Jake Orton <116884079+Jxkeorton@users.noreply.github.com>
2024-10-18 15:36:48 +0000

    Add event binding and conditional redering

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,17 @@ import Sidebar from './components/Sidebar';
 import NavBarSimple from './components/NavBarSimple';
 import EventsClass from './components/EventsClass';
 import EventsFunctional from './components/EventsFunctional';
+import EventBinding from './components/EventBinding';
+import ConditionalRenderingClass from './components/ConditionalRenderingClass';
+import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
+      <ConditionalRenderingClass />
+      <ConditionalRenderingFunctional connected={true} />
+      <EventBinding />
       <EventsClass />
       <EventsFunctional />	
       <NavBarSimple />
