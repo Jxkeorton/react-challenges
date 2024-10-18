884f7f0ecfb383e94463d772b073118cb5bde22b
Jake Orton <116884079+Jxkeorton@users.noreply.github.com>
2024-10-18 20:22:14 +0000

    challenge lifecycles

---
+++ my-app/src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import css from './Loader.module.css'
+
+function Loader() {
+  return (
+    <div className={css.Loader}>
+      
+    </div>
+  )
+}
+
+export default Loader
