21f4c926921e4f6603f64470dd5ef8544dfc5365
Jake Orton <116884079+Jxkeorton@users.noreply.github.com>
2024-10-18 16:52:24 +0000

    methods as props

---
+++ my-app/src/components/MethodsAsPropsParent.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import MethodsAsPropsChild from './MethodsAsPropsChild'
+
+class MethodsAsPropsParent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoggedIn: true,
+        }
+    }
+
+    handleSignIn = () => {
+        this.setState({ isLoggedIn: true })
+        console.log(this)
+    }
+
+    handleSignOut = () => {
+        this.setState({ isLoggedIn: false })
+        console.log(this)
+    }
+
+    handleLogin = () => {
+        this.setState((prevState) => ({
+            isLoggedIn: !prevState.isLoggedIn
+        }));
+    };
+
+    render() {
+        return (
+            <div>
+                <MethodsAsPropsChild 
+                    isLoggedIn={this.state.isLoggedIn} 
+                    handleSignIn={this.handleSignIn}
+                    handleSignOut={this.handleSignOut}
+                />
+            </div>
+        )
+    }
+}
+
+export default MethodsAsPropsParent
