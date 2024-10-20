e61773c7745a6fd56ae79d8bcb0c6020f5928318
Jake Orton <116884079+Jxkeorton@users.noreply.github.com>
2024-10-20 08:50:05 +0000

    UseState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,10 @@
 import css from './App.module.css';
-import Sidebar from './components/Sidebar';
-import NavBarSimple from './components/NavBarSimple';
-import EventsClass from './components/EventsClass';
-import EventsFunctional from './components/EventsFunctional';
-import EventBinding from './components/EventBinding';
-import ConditionalRenderingClass from './components/ConditionalRenderingClass';
-import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
 import NestingComponents from './components/NestingComponents';
 
 function App() {
   return (
     <div className={css.App}>
-      {/* Add your components here */}
       <NestingComponents />
-      {/* <ConditionalRenderingClass />
-      <ConditionalRenderingFunctional connected={true} />
-      <EventBinding />
-      <EventsClass />
-      <EventsFunctional />	
-      <NavBarSimple />
-      <Sidebar /> */}
     </div>
   );
 }
